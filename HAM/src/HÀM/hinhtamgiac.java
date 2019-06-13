d192d0e6886ce251ad73d286f2fc52f6f1064c19
HoangDuyHau <haulb0123@gmail.com>
2019-06-13 16:50:47 +0700

    bt hàm

---
+++ HAM/src/HÀM/hinhtamgiac.java
@@ -0,0 +1,13 @@
+package HÀM;
+import java.util.*;
+class hinhtamgiac {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hinh tam giac");
+		System.out.println("      *     ");
+		System.out.println("    *   *   ");
+		System.out.println("   *******  ");
+	}		
+	}
+
