603f9f2cdd3738a500939ca278b93a2eabcd9dc6
Giang <giang03011998@gmail.com>
2019-06-13 13:45:40 +0700

    bt Mang 2 chiều

---
+++ Mang2chieu/nhapcacphantuchomang.java
@@ -0,0 +1,29 @@
+package Mang2chieu;
+import java.util.Scanner;
+class nhapcacphantuchomang {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int soDong, soCot;	     
+	    Scanner scanner = new Scanner(System.in);
+	    System.out.println("Nhập vào số dòng của mảng: ");
+	    soDong = scanner.nextInt();
+	    System.out.println("Nhập vào số cột của mảng: ");
+	    soCot = scanner.nextInt();	         
+	    int[][] A = new int[soDong][soCot];	         
+	    for (int i = 0; i < soDong; i++) {
+	        for (int j = 0; j < soCot; j++) {
+	            System.out.print("Nhập phần tử thứ [" + i + ", " + j + "]: ");
+	            A[i][j] = scanner.nextInt();
+	        }
+	    }	         	    
+	    System.out.println("Mảng vừa nhập: ");
+	    for (int i = 0; i < soDong; i++) {
+	        for (int j = 0; j < soCot; j++) {
+	            System.out.print(A[i][j] + "\t");
+	        }
+	        System.out.println("\n");   
+	    }
+	}
+
+}
